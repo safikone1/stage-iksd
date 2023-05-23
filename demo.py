8baaff2ee6bbea65a3907a420c312a8b7a0b3419
Safiatou <konesafiatou989@gmail.com>
2023-05-23 09:35:24 +0000

    ligne 9 modifiée

--- demo.py
+++ demo.py
@@ -4,3 +4,6 @@ num1= float(input("entre le premier nombre:"))
 num2= float(input("entre le deuxieme nombre:"))
 resultat=addition(num1,num2)
 print("le resultat de l'addition est:",resultat)
+
+for i in range(1,11):
+    print(i+1)
