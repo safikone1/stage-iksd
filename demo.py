c28ac238b7f6861ef6eadadd2b739b3356299ff8
safikone1 <120180463+safikone1@users.noreply.github.com>
2023-05-23 11:33:18 +0000

    Update and rename demo.py to nouveau.py

--- demo.py
+++
@@ -1,6 +0,0 @@
-def addition(a,b):
-    return a+b
-num1= float(input("entre le premier nombre:"))
-num2= float(input("entre le deuxieme nombre:"))
-resultat=addition(num1,num2)
-print("le resultat de l'addition est:",resultat)
